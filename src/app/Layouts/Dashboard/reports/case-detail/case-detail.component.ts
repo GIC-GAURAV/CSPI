a5bfb1ed7fcb9b93fbd73aa5cd5cfbd53d38f265
utkarsh1205 <utkarsh.malviya@gridinfocom.com>
2022-01-04 19:06:22 +0530

    gauge-added

--- src/app/Layouts/Dashboard/reports/case-detail/case-detail.component.ts
+++ src/app/Layouts/Dashboard/reports/case-detail/case-detail.component.ts
@@ -16,6 +16,49 @@ export class CaseDetailComponent implements OnInit {
     "Tennis",
     "Volleyball",
   ];
+  dataSource = {
+    "chart": {
+        "caption": "",
+        "lowerLimit": "0",
+        "upperLimit": "100",
+        "showValue": "0",
+        "numberSuffix": " ",
+        "theme": "fusion",
+        "showToolTip": "0",
+        "showTickMarks": "0",
+        "showTickValues": "0"
+    },
+    "colorRange": {
+        "color": [
+          {
+              "minValue": "0",
+              "maxValue": "20",
+              "code": "#008000"
+          }, {
+              "minValue": "20",
+              "maxValue": "40",
+              "code": "#CCFC35"
+          }, {
+              "minValue": "40",
+              "maxValue": "60",
+              "code": "#F6FC35"
+          }, {
+              "minValue": "60",
+              "maxValue": "80",
+              "code": "#FCBA35"
+          }, {
+              "minValue": "80",
+              "maxValue": "100",
+              "code": "#FF0000"
+          }
+        ]
+    },
+    "dials": {
+          "dial": [{
+              "value": "50"
+          }]
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
