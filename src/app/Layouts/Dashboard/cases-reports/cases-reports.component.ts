d14cd91c4b1655f585929260535abc9bbc18cc02
gaurav <gaurav@appindia.co.in>
2022-01-20 17:05:23 +0530

    Updated

--- src/app/Layouts/Dashboard/cases-reports/cases-reports.component.ts
+++ src/app/Layouts/Dashboard/cases-reports/cases-reports.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CasesReportsComponent implements OnInit {
   public opened = false;
-
+  carousel : any
+  content : any
+  next: any
+  prev: any
+  item : any
+  selectedId : any
   public gridData: any[] = [
     {
       ProductID: 1,
@@ -48,9 +53,143 @@ export class CasesReportsComponent implements OnInit {
       url: "https://www.telerik.com/kendo-angular-ui-develop/components/layout/card/assets/rose_festival.jpg",
     },
   ];
+
+  public cardData : any = [
+    {
+      "id" : 1,
+      "title" : "Case Creation",
+      "caseCount" : 1,
+      "type" : "Exception",
+      "received" : 300,
+      "processed" : 300,
+      "queue" : 1
+    },
+    {
+      "id" : 2,
+      "title" : "Case Creation",
+      "caseCount" : 2,
+      "type" : "Exception",
+      "received" : 300,
+      "processed" : 300,
+      "queue" : 1
+    },
+    {
+      "id" : 3,
+      "title" : "Case Creation",
+      "caseCount" : 3,
+      "type" : "Exception",
+      "received" : 300,
+      "processed" : 300,
+      "queue" : 1
+    },
+    {
+      "id" : 4,
+      "title" : "Case Creation",
+      "caseCount" : 4,
+      "type" : "Exception",
+      "received" : 300,
+      "processed" : 300,
+      "queue" : 1
+    },
+    {
+      "id" : 5,
+      "title" : "Case Creation",
+      "caseCount" : 5,
+      "type" : "Exception",
+      "received" : 300,
+      "processed" : 300,
+      "queue" : 1
+    },
+    {
+      "id" : 6,
+      "title" : "Case Creation",
+      "caseCount" : 6,
+      "type" : "Exception",
+      "received" : 300,
+      "processed" : 300,
+      "queue" : 1
+    },
+    {
+      "id" : 7,
+      "title" : "Case Creation",
+      "caseCount" : 7,
+      "type" : "Exception",
+      "received" : 300,
+      "processed" : 300,
+      "queue" : 1
+    },
+    {
+      "id" : 8,
+      "title" : "Case Creation",
+      "caseCount" : 8,
+      "type" : "Exception",
+      "received" : 300,
+      "processed" : 300,
+      "queue" : 1
+    },
+    {
+      "id" : 9,
+      "title" : "Case Creation",
+      "caseCount" : 9,
+      "type" : "Exception",
+      "received" : 300,
+      "processed" : 300,
+      "queue" : 1
+    },
+    {
+      "id" : 10,
+      "title" : "Case Creation",
+      "caseCount" : 10,
+      "type" : "Exception",
+      "received" : 300,
+      "processed" : 300,
+      "queue" : 1
+    },
+    {
+      "id" : 11,
+      "title" : "Case Creation",
+      "caseCount" : 11,
+      "type" : "Exception",
+      "received" : 300,
+      "processed" : 300,
+      "queue" : 1
+    },
+    {
+      "id" : 12,
+      "title" : "Case Creation",
+      "caseCount" : 12,
+      "type" : "Exception",
+      "received" : 300,
+      "processed" : 300,
+      "queue" : 1
+    },
+    {
+      "id" : 13,
+      "title" : "Case Creation",
+      "caseCount" : 13,
+      "type" : "Exception",
+      "received" : 300,
+      "processed" : 300,
+      "queue" : 1
+    },
+    {
+      "id" : 14,
+      "title" : "Case Creation",
+      "caseCount" : 14,
+      "type" : "Exception",
+      "received" : 300,
+      "processed" : 300,
+      "queue" : 1
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
+    this.slider();
+  }
+
+  onSelect(id : any){
+    this.selectedId = id
   }
 
   public close() {
@@ -61,4 +200,37 @@ export class CasesReportsComponent implements OnInit {
     this.opened = true;
   }
 
+  slider(){
+    const gap = 16;
+
+    this.carousel = document.getElementById("carousel")
+    this.content = document.getElementById("content")
+    this.next = document.getElementById("next")
+    this.prev = document.getElementById("prev")
+    this.item = document.getElementById('id1')
+    
+    this.next.addEventListener("click", (e :  any) => {
+      this.carousel.scrollBy(width + gap, 0);
+    if (this.carousel.scrollWidth !== 0) {
+      this.prev.style.display = "flex";
+    }
+    if (this.content.scrollWidth - width - gap <= this.carousel.scrollLeft + width) {
+      this.next.style.display = "none";
+    }
+  });
+  this.prev.addEventListener("click", (e : any) => {
+    this.carousel.scrollBy(-(width + gap), 0);
+    if (this.carousel.scrollLeft - width - gap <= 0) {
+      this.prev.style.display = "none";
+    }
+    if (this.content.scrollWidth - width - gap <= this.carousel.scrollLeft + width) {
+      this.next.style.display = "flex";
+    }
+  });
+
+  let width = this.carousel.offsetWidth;
+  window.addEventListener("resize", (e) => (width = this.carousel.offsetWidth));
+  }
 }
+
+
